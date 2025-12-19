0fe37a7c6b0b35915a813b7ad31a91b15d255900
kyj <hong@example.com>
2025-12-19 12:25:52 +0900

    Fix: CI 환경에서 Redis 서비스 연결 설정 추가

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from redis import Redis
 
 app = Flask(__name__)
 
-redis_host = os.getenv('REDIS_HOST', 'localhost')
+redis_host = os.getenv('REDIS_HOST', 'redis')
 redis_port = int(os.getenv('REDIS_PORT', 6379))
 
 redis = Redis(host=redis_host, port=redis_port)
